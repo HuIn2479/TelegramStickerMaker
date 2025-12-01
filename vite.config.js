43c846782700380f56ddbf839a7b044590b55b95
HuIn2479 <66163050+HuIn2479@users.noreply.github.com>
2025-12-01 13:49:41 +0800

    更新 Vite 配置，确保在 GitHub Actions 环境中正确设置基础路径

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/TelegramStickerMaker/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
